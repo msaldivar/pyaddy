cf58792243e67a0ef6a0dd61c9bd4db34fab9ee3
Maurice Saldivar <maurice.a.saldivar@gmail.com>
2023-11-06 14:32:05 -0700

    Basic project structure

---
+++ pyaddy/src/__main__.py
@@ -0,0 +1,27 @@
+"""
+Pyaddy
+
+Maurice Saldivar
+
+"""
+
+from addy import Addy
+
+
+
+def main() -> None:
+    """"""
+    addy_client = Addy()
+
+
+    resp = addy_client.get_api_token_details()
+    print(f'api details {resp}')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ pyaddy/src/addy.py
@@ -0,0 +1,53 @@
+"""
+Class for the addy.io python bindings
+"""
+
+
+
+import requests
+import json
+import configparser
+import os
+
+class Addy:
+    URL = "https://app.addy.io/api/v1/"
+
+    def __init__(self) -> None:
+        """Read key from .cfg file throw error if it can't be found."""
+        cfg = configparser.ConfigParser()
+        cfg.read('env.cfg')
+
+        self.api_key = cfg.get('KEYS', 'api_key')
+        if not self.api_key:
+            raise('No api key present')
+    
+    def _get_headers(self) -> dict:
+        """Return headers used for request.
+        
+        Return:
+            Dict[str]
+        """
+        headers = {
+        'Authorization': f'Bearer {self.api_key}',
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+        }
+
+        return headers
+        
+    def get_api_token_details(self) -> dict[str]:
+        """Return details about the supplied api key.
+
+
+        Returns:
+          If the key is valid, json with three keys will be returned  
+          {'name': 'Batman', 'created_at': '1939-05-28 01:27:00', 'expires_at': '2024-12-12 01:11:11'}
+  
+          If the key is invalid you'll get an unauth error
+        """
+
+        response = requests.request('GET', Addy.URL+'api-token-details', 
+                                    headers=self._get_headers())
+        
+        return response.json()
+        
