2fe0967fe3089f859fb16a2cc61e0cb65b9c6fb1
Maurice Saldivar <maurice.a.saldivar@gmail.com>
2023-11-19 18:35:47 -0700

    Moving cli structure to be:
    Usage: addy <command> <subcommand> [options & parameters]
    
    api command is the first version of this refactor

--- src/pyaddy/commands/api_details_cmd.py
+++ src/pyaddy/commands/api_details_cmd.py
@@ -7,14 +7,18 @@ import click
 import json
 from pyaddy.API.api_details import AddyApiDetails
 
-@click.command(name='get-api-details', help='Get api details to confirm key is valid')
+@click.group()
+def api():
+    """Invoke details about the provided api key"""
+
+@api.command(name='api-details')
 def check_api_key_details():
     """Check the details of the api key"""
     resp = AddyApiDetails().get_api_details()
 
     click.echo(f'API details: {json.dumps(resp.json(), indent=4)}')
 
-@click.command(name="get-account-details", short_help="Get all account details associated with api key")
+@api.command(name='account-details')
 def get_account_details():
     """Get all account details associated with api key"""
     resp = AddyApiDetails().get_account_details()
