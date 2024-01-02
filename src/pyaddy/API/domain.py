61fc09be397c60833a0fc6f0e2b777ec02bb6733
Maurice Saldivar <maurice.a.saldivar@gmail.com>
2024-01-01 20:58:50 -0700

    Add new domain cmd
    * Add cmd /domain-options

--- src/pyaddy/API/domain.py
+++ src/pyaddy/API/domain.py
@@ -10,3 +10,11 @@ class Domain:
     def __init__(self) -> None:
         self.url = AddyURL()
         self.api_key = AddyKey().load_key()
+
+    def get_all_domain_options(self) -> dict:
+        """Retrieve all domain options"""
+
+        response = requests.request(
+            "GET", self.url.get_all_domain_options(), headers=self.url.get_headers(self.api_key)
+        )
+        return response
